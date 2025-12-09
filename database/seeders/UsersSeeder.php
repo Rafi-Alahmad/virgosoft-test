6d1f1080039c1da336012c531e32ba17ad5a371c
Rafi Alahmad <30978201+rafi-al-ahmad@users.noreply.github.com>
2025-12-09 18:46:15 +0300

    update users seeder to create Assets for the user (for test)

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Asset;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -23,15 +24,32 @@ class UsersSeeder extends Seeder
                 "email" => env('ADMIN_EMAIL', 'admin@admin.com'),
                 "email_verified_at" => Carbon::now(),
                 "password" => Hash::make(env('ADMIN_AUTH', '12345678')),
+                'assets' => [
+                    [
+                        'symbol' => 'BTC',
+                        'amount' => 5,
+                    ],
+                    [
+                        'symbol' => 'ETH',
+                        'amount' => 10,
+                    ],
+                ],
             ]
         ];
 
         foreach ($users as $user) {
-            $user = User::firstOrCreate(["email" => $user['email'],], [
+            $userModel = User::firstOrCreate(["email" => $user['email'],], [
                 "name" => $user['name'],
                 "email_verified_at" => $user['email_verified_at'],
                 "password" => $user['password'],
             ]);
+
+            foreach ($user['assets'] as $asset) {
+                Asset::firstOrCreate(['user_id' => $userModel->id, 'symbol' => $asset['symbol']], [
+                    'amount' => $asset['amount'],
+                    'locked_amount' => 0,
+                ]);
+            }
         }
     }
 }
