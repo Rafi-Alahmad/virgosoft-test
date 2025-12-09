92fce72bfa90995a2e3209ef6fb25cb07416553a
Rafi Alahmad <30978201+rafi-al-ahmad@users.noreply.github.com>
2025-12-09 13:26:32 +0300

    create user's assets model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -47,5 +48,10 @@ class User extends Authenticatable
             'password' => 'hashed',
             'balance' => 'decimal:2',
         ];
+    }
+
+    public function assets(): HasMany
+    {
+        return $this->hasMany(Asset::class);
     }
 }
